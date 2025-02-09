c2d38316992dc3fd2e210560bcafc73e5f8f1018
Aktustar <szeverbius@gmail.com>
2025-02-09 21:02:54 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
 function switchimage() { let img = document.getElementById("image");
                         img.src = img.src.includes("images/image1.jpg") ? "images/image2.jpg" : "images/image1.jpg"; }
 document.getElementById("toggle-btn").addEventListener("click",switchimage);    
-
 function togglebackground() { document.body.classList.toggle("dark-mode"); }
 document.getElementById("toggle-bgb").addEventListener("click",togglebackground);
+window.addEventListener("DOMContentLoaded", function() {
+    let music = document.getElementById("background-music");
+    music.play().catch(error => console.log("Autoplay blocked by browser:", error));
+});
