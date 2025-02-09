95b9ddfe5065c32c1c1e23f8e9513442d1292ab7
Aktustar <szeverbius@gmail.com>
2025-02-09 20:48:04 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,5 @@
 function switchimage() { let img = document.getElementById("image");
                         img.src = img.src.includes("images/image1.jpg") ? "images/image2.jpg" : "images/image1.jpg"; }
 document.getElementById("toggle-btn").addEventListener("click",switchimage);    
+
+function togglebackground() { document.body.classList.toggle("dark-mode"); }
